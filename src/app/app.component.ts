967fe6464148e06340e9eef52cc04b9a223eb752
akhil-rajarao <akhil.rajarao@xelpmoc.in>
2024-02-07 15:58:07 +0530

    ngFor Directive applied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,42 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-
   title: string = 'Demo-Application';
   titles: string = 'New application initilisation';
   timer: string = new Date().toLocaleTimeString();
   buttonhide: boolean = false;
   isactive: boolean = true;
   path: string = '../assets/img2.jpg';
-  grass:boolean = false
-  visible: boolean = false
-  counter :number = 0
-  city: string = "Hyderabad"
-  name:any = "(input)=changeName($event)"
-  increment = ()=>{
-this.counter = this.counter + 1
-}
-decrement = ()=>{
-  this.counter = this.counter -= 1
-}
+  grass: boolean = false;
+  visible: boolean = false;
+  counter: number = 0;
+  city: string = 'Hyderabad';
+  movies = ['marvel', 'ironman', 'doctor Strange'];
+  name: any = '(input)=changeName($event)';
+  increment = () => {
+    this.counter = this.counter + 1;
+  };
+  decrement = () => {
+    this.counter = this.counter -= 1;
+  };
 
-onchangeFunction =(e:any)=>{
- this.name = e.target.value
-}
+  onchangeFunction = (e: any) => {
+    this.name = e.target.value;
+  };
 
-inputchangeFUnction = (e:any)=>{
-  this.city = e.target.value
-}
+  inputchangeFUnction = (e: any) => {
+    this.city = e.target.value;
+  };
 
-changecity = ()=>{
-  this.city = this.city == "Bangalore"  ? "Hyderabad" : "Bangalore"
-}
+  changecity = () => {
+    this.city = this.city == 'Bangalore' ? 'Hyderabad' : 'Bangalore';
+  };
 
-changeVisibility =  ()=>{
-  this.visible = !this.visible
-}
-
+  changeVisibility = () => {
+    this.visible = !this.visible;
+  };
 }
