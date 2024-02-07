afb0f0d7a3c026f5d77d5d0e78b0208cf94edebe
akhil-rajarao <akhil.rajarao@xelpmoc.in>
2024-02-07 15:18:36 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularConcepts2';
+
+  title: string = 'Demo-Application';
+  titles: string = 'New application initilisation';
+  timer: string = new Date().toLocaleTimeString();
+  buttonhide: boolean = false;
+  isactive: boolean = true;
+  path: string = '../assets/img2.jpg';
+  grass:boolean = false
+  counter :number = 0
+  city: string = "Hyderabad"
+  name:any = "(input)=changeName($event)"
+  increment = ()=>{
+this.counter = this.counter + 1
+}
+decrement = ()=>{
+  this.counter = this.counter -= 1
+}
+
+onchangeFunction =(e:any)=>{
+ this.name = e.target.value
+}
+
+inputchangeFUnction = (e:any)=>{
+  this.city = e.target.value
+}
+
+changecity = ()=>{
+  this.city = this.city == "Bangalore" ? "Hyderabad" : "Bangalore"
+}
+
 }
