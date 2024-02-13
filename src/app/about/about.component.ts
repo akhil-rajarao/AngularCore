2b2bafcd9eafbf86841cad9ae8cafad9eea6f20e
akhil-rajarao <akhil.rajarao@xelpmoc.in>
2024-02-13 19:14:00 +0530

    query params applied for an anchor tag

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,17 +8,27 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './about.component.css',
 })
 export class AboutComponent implements OnInit {
+  queryName: any = 'nvrerverve';
   constructor(
     private route: ActivatedRoute,
     private newts: TestServiceService
   ) {}
   uservalue: any;
+  usernames: any;
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       let userId = params.get('id');
+      let dsbsdgb = params.get('name');
+      if (dsbsdgb) {
+        this.queryName = dsbsdgb;
+      }
+
+      this.queryName = dsbsdgb;
       console.log(userId);
+      console.log(this.queryName);
       this.uservalue = userId;
+      this.usernames = this.newts.userDetails.find((e) => e.id == userId);
     });
   }
   newMobiles = this.newts.mobileName;
