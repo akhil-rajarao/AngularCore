9d6a276c10e05185ebc362698bd7a5164d8849be
akhil-rajarao <akhil.rajarao@xelpmoc.in>
2024-02-21 16:46:37 +0530

    added custom validation function and added error based on user input

--- src/app/form-component/form-component.component.ts
+++ src/app/form-component/form-component.component.ts
@@ -4,15 +4,26 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form-component',
   templateUrl: './form-component.component.html',
-  styleUrl: './form-component.component.css'
+  styleUrl: './form-component.component.css',
 })
 export class FormComponentComponent {
-  constructor(private fb:FormBuilder){
+  constructor(private fb: FormBuilder) {}
 
-  }
-signup= this.fb.group({
-  user:this.fb.control('akhil'),
-  mail:[],
-  pwd:[null,Validators.required]
-})
+  customValidation = (a: any) => {
+    let pattern = /[A-Z]/;
+    if (pattern.test(a.value)) {
+      return null;
+    }
+    return { noUpper: true };
+  };
+  signup = this.fb.group({
+    user: this.fb.control(null, this.customValidation),
+    mail: [],
+    pwd: [null, Validators.required],
+  });
+  errormsg = this.signup.controls.user.errors?.['noUpper'] == true;
+
+  show = () => {
+    console.log(this.signup.controls.user.errors?.['noUpper']);
+  };
 }
